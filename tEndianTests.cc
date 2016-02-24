b55fa8fe19f244b35985c306a55ddf20ce4ff074
TJ Grant <tjgrant@tatewake.com>
2016-02-23 19:51:25 -0800

    endian_template - Converted tEndianTests from a MiniCCUnit unit test class to a stand-alone class and a main method to call it; also added print statements to tell the user what tests are passing

--- tEndianTests.cc
+++ tEndianTests.cc
@@ -25,46 +25,47 @@
  SOFTWARE.
  */
 
-#include "Base/package.h"
+#include <assert.h>
+#include "tEndian.h"
 
-#include "Test/package.h"
+#include <stdio.h>
 
-class tEndianTests : public TestFixture<tEndianTests>
+class tEndianTests
 {
 public:
-	TEST_FIXTURE( tEndianTests )
+	tEndianTests()
 	{
-		TEST_CASE( testSizeOf );
-		TEST_CASE( testPlatMemIsMem );
-		TEST_CASE( testOtherMemIsSwapped );
-		TEST_CASE( testArithmeticAssignment );
-		TEST_CASE( testLittleCompoundArithmeticAssignment );
-		TEST_CASE( testBigCompoundArithmeticAssignment );
-        TEST_CASE( testBigIsBig );
-        TEST_CASE( testLittleIsLittle );
+        testSizeOf();
+		testPlatMemIsMem();
+		testOtherMemIsSwapped();
+		testArithmeticAssignment();
+		testLittleCompoundArithmeticAssignment();
+		testBigCompoundArithmeticAssignment();
+        testBigIsBig();
+        testLittleIsLittle();
 	}
 
     void testSizeOf()
 	{
-		ASSERT(sizeof(leuint64_t) == 8);
-		ASSERT(sizeof(leuint32_t) == 4);
-		ASSERT(sizeof(leuint16_t) == 2);
-		ASSERT(sizeof(leint64_t) == 8);
-		ASSERT(sizeof(leint32_t) == 4);
-		ASSERT(sizeof(leint16_t) == 2);
-        ASSERT(sizeof(lefloat64_t) == 8);
-        ASSERT(sizeof(lefloat32_t) == 4);
-        ASSERT(sizeof(lefloat16_t) == 2);
+		assert(sizeof(leuint64_t) == 8);
+		assert(sizeof(leuint32_t) == 4);
+		assert(sizeof(leuint16_t) == 2);
+		assert(sizeof(leint64_t) == 8);
+		assert(sizeof(leint32_t) == 4);
+		assert(sizeof(leint16_t) == 2);
+        assert(sizeof(lefloat64_t) == 8);
+        assert(sizeof(lefloat32_t) == 4);
 
-		ASSERT(sizeof(beuint64_t) == 8);
-		ASSERT(sizeof(beuint32_t) == 4);
-		ASSERT(sizeof(beuint16_t) == 2);
-		ASSERT(sizeof(beint64_t) == 8);
-		ASSERT(sizeof(beint32_t) == 4);
-		ASSERT(sizeof(beint16_t) == 2);
-        ASSERT(sizeof(befloat64_t) == 8);
-        ASSERT(sizeof(befloat32_t) == 4);
-        ASSERT(sizeof(befloat16_t) == 2);
+		assert(sizeof(beuint64_t) == 8);
+		assert(sizeof(beuint32_t) == 4);
+		assert(sizeof(beuint16_t) == 2);
+		assert(sizeof(beint64_t) == 8);
+		assert(sizeof(beint32_t) == 4);
+		assert(sizeof(beint16_t) == 2);
+        assert(sizeof(befloat64_t) == 8);
+        assert(sizeof(befloat32_t) == 4);
+
+        printf("*** ::testSizeOf passed\n");
     }
 
     void testPlatMemIsMem()
@@ -80,13 +81,15 @@ public:
         if (u.c[0])
         {
             leuint32_t val = 0x12345678;
-            ASSERT(((uint32_t*)&val)[0] == 0x12345678);
+            assert(((uint32_t*)&val)[0] == 0x12345678);
         }
         else
         {
             beuint32_t val = 0x12345678;
-            ASSERT(((uint32_t*)&val)[0] == 0x12345678);
+            assert(((uint32_t*)&val)[0] == 0x12345678);
         }
+
+        printf("*** ::testPlatMemIsMem passed\n");
 	}
 
     void testOtherMemIsSwapped()
@@ -102,13 +105,15 @@ public:
         if (u.c[0])
         {
             beuint32_t val = 0x12345678;
-            ASSERT(((uint32_t*)&val)[0] == 0x78563412);
+            assert(((uint32_t*)&val)[0] == 0x78563412);
         }
         else
         {
             leuint32_t val = 0x12345678;
-            ASSERT(((uint32_t*)&val)[0] == 0x78563412);
+            assert(((uint32_t*)&val)[0] == 0x78563412);
         }
+
+        printf("*** ::testOtherMemIsSwapped passed\n");
 	}
 
     void testArithmeticAssignment()
@@ -119,58 +124,64 @@ public:
         // Prefix ++
         a = 0; b = 0;
 
-        ASSERT(a == 0);     ASSERT(b == 0);
-        ASSERT(++a == 1);   ASSERT(++b == 1);
-        ASSERT(a == 1);     ASSERT(b == 1);
+        assert(a == 0);     assert(b == 0);
+        assert(++a == 1);   assert(++b == 1);
+        assert(a == 1);     assert(b == 1);
 
         // Prefix --
         a = 0; b = 0;
 
-        ASSERT(a == 0);     ASSERT(b == 0);
-        ASSERT(--a == -1);  ASSERT(--b == -1);
-        ASSERT(a == -1);    ASSERT(b == -1);
+        assert(a == 0);     assert(b == 0);
+        assert(--a == -1);  assert(--b == -1);
+        assert(a == -1);    assert(b == -1);
 
         // Postfix ++
         a = 0; b = 0;
 
-        ASSERT(a++ == 0);   ASSERT(b++ == 0);
-        ASSERT(a == 1);     ASSERT(b == 1);
+        assert(a++ == 0);   assert(b++ == 0);
+        assert(a == 1);     assert(b == 1);
 
         // Postfix --
         a = 0; b = 0;
 
-        ASSERT(a-- == 0);   ASSERT(b-- == 0);
-        ASSERT(a == -1);    ASSERT(b == -1);
+        assert(a-- == 0);   assert(b-- == 0);
+        assert(a == -1);    assert(b == -1);
+
+        printf("*** ::testArithmeticAssignment passed\n");
     }
 
     void testLittleCompoundArithmeticAssignment()
 	{
         leint16_t a;
-        a = 2; a += 5; ASSERT(a == 7);
-        a = 2; a -= 5; ASSERT(a == -3);
-        a = 2; a *= 5; ASSERT(a == 10);
-        a = 15; a /= 5; ASSERT(a == 3);
-        a = 13; a %= 7; ASSERT(a == 6);
-        a = 63; a &= 7; ASSERT(a == 7);
-        a = 63; a |= 64; ASSERT(a == 127);
-        a = 63; a ^= 7; ASSERT(a == 56);
-        a = 63; a <<= 1; ASSERT(a == 126);
-        a = 126; a >>= 1; ASSERT(a == 63);
+        a = 2; a += 5; assert(a == 7);
+        a = 2; a -= 5; assert(a == -3);
+        a = 2; a *= 5; assert(a == 10);
+        a = 15; a /= 5; assert(a == 3);
+        a = 13; a %= 7; assert(a == 6);
+        a = 63; a &= 7; assert(a == 7);
+        a = 63; a |= 64; assert(a == 127);
+        a = 63; a ^= 7; assert(a == 56);
+        a = 63; a <<= 1; assert(a == 126);
+        a = 126; a >>= 1; assert(a == 63);
+
+        printf("*** ::testLittleCompoundArithmeticAssignment passed\n");
 	}
 
     void testBigCompoundArithmeticAssignment()
 	{
         beint16_t a;
-        a = 2; a += 5; ASSERT(a == 7);
-        a = 2; a -= 5; ASSERT(a == -3);
-        a = 2; a *= 5; ASSERT(a == 10);
-        a = 15; a /= 5; ASSERT(a == 3);
-        a = 13; a %= 7; ASSERT(a == 6);
-        a = 63; a &= 7; ASSERT(a == 7);
-        a = 63; a |= 64; ASSERT(a == 127);
-        a = 63; a ^= 7; ASSERT(a == 56);
-        a = 63; a <<= 1; ASSERT(a == 126);
-        a = 126; a >>= 1; ASSERT(a == 63);
+        a = 2; a += 5; assert(a == 7);
+        a = 2; a -= 5; assert(a == -3);
+        a = 2; a *= 5; assert(a == 10);
+        a = 15; a /= 5; assert(a == 3);
+        a = 13; a %= 7; assert(a == 6);
+        a = 63; a &= 7; assert(a == 7);
+        a = 63; a |= 64; assert(a == 127);
+        a = 63; a ^= 7; assert(a == 56);
+        a = 63; a <<= 1; assert(a == 126);
+        a = 126; a >>= 1; assert(a == 63);
+
+        printf("*** ::testBigCompoundArithmeticAssignment passed\n");
 	}
 
     void testBigIsBig()
@@ -181,11 +192,11 @@ public:
             leuint64_t   lit     = 0x1122334455667788;
             beuint64_t   other   = 0x1122334455667788;
 
-            ASSERT( ((uint64_t*)&big)[0] != ((uint64_t*)&invbig)[0]);
-            ASSERT( ((uint64_t*)&big)[0] == ((uint64_t*)&other)[0] );
-            ASSERT( ((uint64_t*)&big)[0] != ((uint64_t*)&lit)[0] );
-            ASSERT( ((uint64_t*)&invbig)[0] != ((uint64_t*)&other)[0] );
-            ASSERT( ((uint64_t*)&invbig)[0] == ((uint64_t*)&lit)[0] );
+            assert( ((uint64_t*)&big)[0] != ((uint64_t*)&invbig)[0]);
+            assert( ((uint64_t*)&big)[0] == ((uint64_t*)&other)[0] );
+            assert( ((uint64_t*)&big)[0] != ((uint64_t*)&lit)[0] );
+            assert( ((uint64_t*)&invbig)[0] != ((uint64_t*)&other)[0] );
+            assert( ((uint64_t*)&invbig)[0] == ((uint64_t*)&lit)[0] );
         }
         {
             beint32_t   big     = 0x12345678;
@@ -193,11 +204,11 @@ public:
             leint32_t   lit     = 0x12345678;
             beint32_t   other   = 0x12345678;
 
-            ASSERT( ((uint32_t*)&big)[0] != ((uint32_t*)&invbig)[0]);
-            ASSERT( ((uint32_t*)&big)[0] == ((uint32_t*)&other)[0] );
-            ASSERT( ((uint32_t*)&big)[0] != ((uint32_t*)&lit)[0] );
-            ASSERT( ((uint32_t*)&invbig)[0] != ((uint32_t*)&other)[0] );
-            ASSERT( ((uint32_t*)&invbig)[0] == ((uint32_t*)&lit)[0] );
+            assert( ((uint32_t*)&big)[0] != ((uint32_t*)&invbig)[0]);
+            assert( ((uint32_t*)&big)[0] == ((uint32_t*)&other)[0] );
+            assert( ((uint32_t*)&big)[0] != ((uint32_t*)&lit)[0] );
+            assert( ((uint32_t*)&invbig)[0] != ((uint32_t*)&other)[0] );
+            assert( ((uint32_t*)&invbig)[0] == ((uint32_t*)&lit)[0] );
         }
         {
             beint16_t   big     = 0x0102;
@@ -205,12 +216,14 @@ public:
             leint16_t   lit     = 0x0102;
             beint16_t   other   = 0x0102;
 
-            ASSERT( ((uint16_t*)&big)[0] != ((uint16_t*)&invbig)[0]);
-            ASSERT( ((uint16_t*)&big)[0] == ((uint16_t*)&other)[0] );
-            ASSERT( ((uint16_t*)&big)[0] != ((uint16_t*)&lit)[0] );
-            ASSERT( ((uint16_t*)&invbig)[0] != ((uint16_t*)&other)[0] );
-            ASSERT( ((uint16_t*)&invbig)[0] == ((uint16_t*)&lit)[0] );
+            assert( ((uint16_t*)&big)[0] != ((uint16_t*)&invbig)[0]);
+            assert( ((uint16_t*)&big)[0] == ((uint16_t*)&other)[0] );
+            assert( ((uint16_t*)&big)[0] != ((uint16_t*)&lit)[0] );
+            assert( ((uint16_t*)&invbig)[0] != ((uint16_t*)&other)[0] );
+            assert( ((uint16_t*)&invbig)[0] == ((uint16_t*)&lit)[0] );
         }
+
+        printf("*** ::testBigIsBig passed\n");
     }
 
     void testLittleIsLittle()
@@ -221,11 +234,11 @@ public:
             leuint64_t   plat    = 0x1122334455667788;
             beuint64_t   big     = 0x1122334455667788;
 
-            ASSERT( ((uint32_t*)&lit)[0] != ((uint32_t*)&invlit)[0]);
-            ASSERT( ((uint32_t*)&lit)[0] != ((uint32_t*)&big)[0] );
-            ASSERT( ((uint32_t*)&lit)[0] == ((uint32_t*)&plat)[0] );
-            ASSERT( ((uint32_t*)&invlit)[0] == ((uint32_t*)&big)[0] );
-            ASSERT( ((uint32_t*)&invlit)[0] != ((uint32_t*)&plat)[0] );
+            assert( ((uint32_t*)&lit)[0] != ((uint32_t*)&invlit)[0]);
+            assert( ((uint32_t*)&lit)[0] != ((uint32_t*)&big)[0] );
+            assert( ((uint32_t*)&lit)[0] == ((uint32_t*)&plat)[0] );
+            assert( ((uint32_t*)&invlit)[0] == ((uint32_t*)&big)[0] );
+            assert( ((uint32_t*)&invlit)[0] != ((uint32_t*)&plat)[0] );
         }
         {
             leint32_t   lit     = 0x12345678;
@@ -233,11 +246,11 @@ public:
             leint32_t   plat    = 0x12345678;
             beint32_t   big     = 0x12345678;
 
-            ASSERT( ((uint32_t*)&lit)[0] != ((uint32_t*)&invlit)[0]);
-            ASSERT( ((uint32_t*)&lit)[0] != ((uint32_t*)&big)[0] );
-            ASSERT( ((uint32_t*)&lit)[0] == ((uint32_t*)&plat)[0] );
-            ASSERT( ((uint32_t*)&invlit)[0] == ((uint32_t*)&big)[0] );
-            ASSERT( ((uint32_t*)&invlit)[0] != ((uint32_t*)&plat)[0] );
+            assert( ((uint32_t*)&lit)[0] != ((uint32_t*)&invlit)[0]);
+            assert( ((uint32_t*)&lit)[0] != ((uint32_t*)&big)[0] );
+            assert( ((uint32_t*)&lit)[0] == ((uint32_t*)&plat)[0] );
+            assert( ((uint32_t*)&invlit)[0] == ((uint32_t*)&big)[0] );
+            assert( ((uint32_t*)&invlit)[0] != ((uint32_t*)&plat)[0] );
         }
         {
             leint16_t   lit     = 0x0102;
@@ -245,13 +258,24 @@ public:
             leint16_t   plat    = 0x0102;
             beint16_t   big     = 0x0102;
 
-            ASSERT( ((uint16_t*)&lit)[0] != ((uint16_t*)&invlit)[0]);
-            ASSERT( ((uint16_t*)&lit)[0] != ((uint16_t*)&big)[0] );
-            ASSERT( ((uint16_t*)&lit)[0] == ((uint16_t*)&plat)[0] );
-            ASSERT( ((uint16_t*)&invlit)[0] == ((uint16_t*)&big)[0] );
-            ASSERT( ((uint16_t*)&invlit)[0] != ((uint16_t*)&plat)[0] );
+            assert( ((uint16_t*)&lit)[0] != ((uint16_t*)&invlit)[0]);
+            assert( ((uint16_t*)&lit)[0] != ((uint16_t*)&big)[0] );
+            assert( ((uint16_t*)&lit)[0] == ((uint16_t*)&plat)[0] );
+            assert( ((uint16_t*)&invlit)[0] == ((uint16_t*)&big)[0] );
+            assert( ((uint16_t*)&invlit)[0] != ((uint16_t*)&plat)[0] );
         }
+
+        printf("*** ::testLittleIsLittle passed\n");
     }
 };
 
-REGISTER_FIXTURE( tEndianTests );
+int main (int argc, char *argv[])
+{
+    printf("*** Running tEndianTests...\n");
+
+    tEndianTests();
+
+    printf("*** All tests passed!\n");
+
+    return 0;
+}
